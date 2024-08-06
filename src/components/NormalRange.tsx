3391efde8e8e83c6406e8478b5e294185995a1f3
alejandromzMCA <alejandro.munozdelprado@mca-groupe.com>
2024-08-06 17:51:22 +0200

    wip: normal range working

--- src/components/NormalRange.tsx
+++ src/components/NormalRange.tsx
@@ -2,32 +2,44 @@ import { useEffect, useRef, useState } from "react";
 
 const NormalRange = (): JSX.Element => {
     const [updating, setUpdating] = useState(false);
-    const [dragging, setDragging] = useState(false);
+    const [draggingLeft, setDraggingLeft] = useState(false);
+    const [draggingRight, setDraggingRight] = useState(false);
     const [minValue, setMinValue] = useState(0);
     const [maxValue, setMaxValue] = useState(100);
-    const marginLeft = 18;
-    const marginRight = marginLeft + 27.5;
+    const marginLeft = 16;
+    const marginRight = 18;
     const minX = useRef(marginLeft);
     const maxX = useRef(marginRight);
-    const rangeWrapperRef = useRef<HTMLDivElement | null>(null);
-    const x = useRef(marginLeft);
+    const lineRef = useRef<HTMLDivElement | null>(null);
+    const xLeft = useRef(marginLeft);
+    const xRight = useRef(marginLeft);
 
-    const updateX = (newX: number) => {
-        if (x.current !== newX) {
-            x.current = newX;
-            if (!updating) {
-                setUpdating(true);
-                setTimeout(() => setUpdating(false), 100)
-            }
+
+
+    const updateX = ({ newLeftX, newRightX }: { newLeftX?: number; newRightX?: number }) => {
+        let update = false;
+        if (newLeftX && xLeft.current !== newLeftX) {
+            xLeft.current = newLeftX;
+            update = !updating;
+        }
+        if (newRightX && xRight.current !== newRightX) {
+            xRight.current = newRightX;
+            update = !updating;
+        }
+        if (update) {
+            setUpdating(true);
+            setTimeout(() => setUpdating(false), 100)
         }
     }
 
     useEffect(() => {
         const updateLimits = () => {
-            if (rangeWrapperRef.current) {
-                const rect = rangeWrapperRef.current.getBoundingClientRect();
+            if (lineRef.current) {
+                const rect = lineRef.current.getBoundingClientRect();
+                minX.current = rect.left - marginLeft;
                 maxX.current = rect.right - marginRight;
-                updateX(Math.max(Math.min(x.current, maxX.current), minX.current));
+                updateX({ newLeftX: Math.max(Math.min(xLeft.current, xRight.current - 10), minX.current) });
+                updateX({ newRightX: Math.max(Math.min(xRight.current, maxX.current), xLeft.current + 10) });
             }
         }
         updateLimits();
@@ -37,34 +49,39 @@ const NormalRange = (): JSX.Element => {
         }
     }, [])
 
-    const handleMouseDown = () => {
-        setDragging(true);
+    const handleMouseDownLeft = () => {
+        setDraggingLeft(true);
+    }
+    const handleMouseDownRight = () => {
+        setDraggingRight(true);
     }
 
     const handleMouseUp = () => {
-        setDragging(false);
+        setDraggingLeft(false);
+        setDraggingRight(false);
     }
 
     const handleMouseMove = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         event.preventDefault();
         event.stopPropagation();
         const newX = event.clientX - 18;
-        if (dragging && x.current !== newX) {
-            updateX(Math.max(Math.min(newX, maxX.current), minX.current));
+        if (draggingLeft && xLeft.current !== newX) {
+            updateX({ newLeftX: Math.max(Math.min(newX, xRight.current - 10), minX.current) });
+        }
+        if (draggingRight && xRight.current !== newX) {
+            updateX({ newRightX: Math.max(Math.min(newX, maxX.current), xLeft.current + 10) });
         }
     }
 
-    return <div ref={rangeWrapperRef} onMouseUp={handleMouseUp} onMouseMove={handleMouseMove} onMouseLeave={handleMouseUp} className="rangeWrapper" style={{ cursor: dragging ? "grabbing" : undefined }}>
-        <div style={{ display: 'flex', flexDirection: 'column', position: 'relative' }}>
-            <div className="rangeLimit" />
-            <input type="text" value={minValue} onChange={(ev) => setMinValue(Number.parseInt(ev.currentTarget.value))} style={{ position: 'absolute', border: '0px', borderRadius: '5px', bottom: '-23px', left: '-20px', width: '40px' }} />
-        </div>
-        <div className="rangeLine" />
-        <div style={{ display: 'flex', flexDirection: 'column', position: 'relative' }}>
-            <div className="rangeLimit" />
-            <input type="text" value={maxValue} onChange={(ev) => setMaxValue(Number.parseInt(ev.currentTarget.value))} style={{ position: 'absolute', border: '0px', borderRadius: '5px', bottom: '-23px', left: '-20px', width: '40px' }} />
-        </div>
-        <div onMouseDown={handleMouseDown} className="rangeDot" style={{ left: x.current, cursor: dragging ? "grabbing" : "grab" }} />
+    const dragging = draggingLeft || draggingRight;
+    return <div onMouseUp={handleMouseUp} onMouseMove={handleMouseMove} onMouseLeave={handleMouseUp} className="rangeWrapper" style={{ cursor: dragging ? "grabbing" : undefined }}>
+        <input type="text" title="Range min limit" value={minValue} onChange={(ev) => setMinValue(Number.parseInt(ev.currentTarget.value))} className="rangeLimitInput" />
+        <span className="rangeLimitLabel">€</span>
+        <div ref={lineRef} className="rangeLine" />
+        <input type="text" title="Range max limit" value={maxValue} onChange={(ev) => setMaxValue(Number.parseInt(ev.currentTarget.value))} className="rangeLimitInput" />
+        <span className="rangeLimitLabel">€</span>
+        <div onMouseDown={handleMouseDownLeft} className="rangeDot" style={{ left: xLeft.current, cursor: dragging ? "grabbing" : "grab" }} />
+        <div onMouseDown={handleMouseDownRight} className="rangeDot" style={{ left: xRight.current, cursor: dragging ? "grabbing" : "grab" }} />
     </div>
 }
 
