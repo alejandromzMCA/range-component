3de21e5e0709125d110cc1c31ac22cec96a6dd7e
alejandromzMCA <alejandro.munozdelprado@mca-groupe.com>
2024-08-07 09:00:23 +0200

    fix: delay problem

--- src/components/NormalRange.tsx
+++ src/components/NormalRange.tsx
@@ -11,24 +11,24 @@ const NormalRange = (): JSX.Element => {
     const minX = useRef(marginLeft);
     const maxX = useRef(marginRight);
     const lineRef = useRef<HTMLDivElement | null>(null);
-    const xLeft = useRef(marginLeft);
-    const xRight = useRef(marginLeft);
+    const leftX = useRef(marginLeft);
+    const rightX = useRef(marginLeft);
 
 
 
     const updateX = ({ newLeftX, newRightX }: { newLeftX?: number; newRightX?: number }) => {
         let update = false;
-        if (newLeftX && xLeft.current !== newLeftX) {
-            xLeft.current = newLeftX;
+        if (newLeftX && leftX.current !== newLeftX) {
+            leftX.current = newLeftX;
             update = !updating;
         }
-        if (newRightX && xRight.current !== newRightX) {
-            xRight.current = newRightX;
+        if (newRightX && rightX.current !== newRightX) {
+            rightX.current = newRightX;
             update = !updating;
         }
         if (update) {
             setUpdating(true);
-            setTimeout(() => setUpdating(false), 100)
+            setTimeout(() => setUpdating(false), 20)
         }
     }
 
@@ -38,8 +38,8 @@ const NormalRange = (): JSX.Element => {
                 const rect = lineRef.current.getBoundingClientRect();
                 minX.current = rect.left - marginLeft;
                 maxX.current = rect.right - marginRight;
-                updateX({ newLeftX: Math.max(Math.min(xLeft.current, xRight.current - 10), minX.current) });
-                updateX({ newRightX: Math.max(Math.min(xRight.current, maxX.current), xLeft.current + 10) });
+                updateX({ newLeftX: Math.max(Math.min(leftX.current, rightX.current - 10), minX.current) });
+                updateX({ newRightX: Math.max(Math.min(rightX.current, maxX.current), leftX.current + 10) });
             }
         }
         updateLimits();
@@ -65,23 +65,23 @@ const NormalRange = (): JSX.Element => {
         event.preventDefault();
         event.stopPropagation();
         const newX = event.clientX - 18;
-        if (draggingLeft && xLeft.current !== newX) {
-            updateX({ newLeftX: Math.max(Math.min(newX, xRight.current - 10), minX.current) });
+        if (draggingLeft && leftX.current !== newX) {
+            updateX({ newLeftX: Math.max(Math.min(newX, rightX.current - 10), minX.current) });
         }
-        if (draggingRight && xRight.current !== newX) {
-            updateX({ newRightX: Math.max(Math.min(newX, maxX.current), xLeft.current + 10) });
+        if (draggingRight && rightX.current !== newX) {
+            updateX({ newRightX: Math.max(Math.min(newX, maxX.current), leftX.current + 10) });
         }
     }
 
     const dragging = draggingLeft || draggingRight;
     return <div onMouseUp={handleMouseUp} onMouseMove={handleMouseMove} onMouseLeave={handleMouseUp} className="rangeWrapper" style={{ cursor: dragging ? "grabbing" : undefined }}>
-        <input type="text" title="Range min limit" value={minValue} onChange={(ev) => setMinValue(Number.parseInt(ev.currentTarget.value))} className="rangeLimitInput" />
+        <input type="text" title="Range min limit" value={minValue} onChange={(ev) => setMinValue((currentValue) => Number.parseInt(ev.currentTarget?.value) || currentValue)} className="rangeLimitInput" />
         <span className="rangeLimitLabel">€</span>
         <div ref={lineRef} className="rangeLine" />
-        <input type="text" title="Range max limit" value={maxValue} onChange={(ev) => setMaxValue(Number.parseInt(ev.currentTarget.value))} className="rangeLimitInput" />
+        <input type="text" title="Range max limit" value={maxValue} onChange={(ev) => setMaxValue((currentValue) => Number.parseInt(ev.currentTarget?.value) || currentValue)} className="rangeLimitInput" />
         <span className="rangeLimitLabel">€</span>
-        <div onMouseDown={handleMouseDownLeft} className="rangeDot" style={{ left: xLeft.current, cursor: dragging ? "grabbing" : "grab" }} />
-        <div onMouseDown={handleMouseDownRight} className="rangeDot" style={{ left: xRight.current, cursor: dragging ? "grabbing" : "grab" }} />
+        <div onMouseDown={handleMouseDownLeft} className="rangeDot" style={{ left: leftX.current, cursor: dragging ? "grabbing" : "grab" }} />
+        <div onMouseDown={handleMouseDownRight} className="rangeDot" style={{ left: rightX.current, cursor: dragging ? "grabbing" : "grab" }} />
     </div>
 }
 
