90c46b0f063abd1427af4d42276e515913a1b3b3
alejandromdp <alejandromdp@ext.inditex.com>
2024-08-09 08:17:16 +0200

    chore: add mock api calls

--- src/pages/exercise1.tsx
+++ src/pages/exercise1.tsx
@@ -1,7 +1,19 @@
 import NormalRange from "@/components/NormalRange"
 
-const Exercise1 = () => {
-    return <div style={{ display: "flex", marginTop: "3rem", justifyContent: "center", alignItems: "center" }}><NormalRange minDefault={0} maxDefault={100} /></div>
+interface Exercise1Props {
+    minDefault: number;
+    maxDefault: number;
+}
+const Exercise1: React.FC<Exercise1Props> = ({ minDefault = 0, maxDefault = 10 }) => {
+    return <div style={{ display: "flex", marginTop: "3rem", justifyContent: "center", alignItems: "center" }}><NormalRange minDefault={minDefault} maxDefault={maxDefault} /></div>
 }
 
+export const getServerSideProps = async () => {
+    const res = await fetch("http://demo8286832.mockable.io/getNormalRange");
+    const data = (await res.json()) as Exercise1Props;
+
+    return {
+        props: data,
+    }
+}
 export default Exercise1
--- src/pages/exercise2.tsx
+++ src/pages/exercise2.tsx
@@ -1,7 +1,19 @@
 import FixedValuesRange from "@/components/FixedValuesRange"
 
-const Exercise2 = () => {
-    return <div style={{ display: "flex", marginTop: "3rem", justifyContent: "center", alignItems: "center" }}><FixedValuesRange rangeLimitsValue={[1.99, 5.99, 10.99, 30.99, 50.99, 70.99]} /></div>
+interface Exercise2Props {
+    rangeLimitsValue: number[];
+}
+const Exercise2: React.FC<Exercise2Props> = ({ rangeLimitsValue }) => {
+    return <div style={{ display: "flex", marginTop: "3rem", justifyContent: "center", alignItems: "center" }}><FixedValuesRange rangeLimitsValue={rangeLimitsValue} /></div>
+}
+
+export const getServerSideProps = async () => {
+    const res = await fetch("http://demo8286832.mockable.io/getFixedValuesRange");
+    const data = (await res.json()) as Exercise2Props;
+
+    return {
+        props: data || [],
+    }
 }
 
 export default Exercise2
